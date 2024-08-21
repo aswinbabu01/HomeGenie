2ed1f8de8ef9bf657d4b910ebb75959ab0b7dde0
aswinbabu01 <mjaswin001@gmail.com>
2024-08-21 18:38:46 +0530

    Latest Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
-  
+    <Navbar/>
     </>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import'../../assets/logo HG.png'
+import '../Navbar/Navbar.css'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+ <nav className="navbar bg-primary" data-bs-theme="dark">
+  <div className="logo d-flex">
+  <img className='d-block w-100' src='assets/logo HG.png' alt='logo'/>
+  <div className="navoption">
+    <ul className='options d-flex mx-5'>
+      <li>Home</li>
+      <li>Category</li>
+      <li>About</li>
+      <li>WorkWithUs</li>
+    </ul>
+  </div>
+  </div>
+</nav>
+   </>
   )
 }
 
